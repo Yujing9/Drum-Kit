1d5193b293610c9c2e8ca3e9cf7155ede6f8f70e
Yujing9 <94563477+Yujing9@users.noreply.github.com>
2022-10-31 16:21:24 +0000

    final

---
+++ index.js
@@ -0,0 +1,55 @@
+for (var i = 0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function (){
+    var listen = this.innerHTML;
+    makeSound(listen);
+    buttonActive(listen);
+    });
+}
+document.addEventListener("keypress",function (event){
+    makeSound(event.key);
+    buttonActive(event.key)
+
+});
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break
+        case "a":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break
+        case "j":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+
+        };
+}
+function buttonActive(currentKey) {
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    },100)
+    
+}
+
+
